32ac22c7831f1b71646599995c7b50567cf793e7
Michael Wilson <soothsayre@hotmail.co.uk>
2013-12-02 09:37:35 +0000

    Added a python test file and a README to the src folder.

---
+++ src/test.py
@@ -0,0 +1,14 @@
+def two_hoch_etwas(i):
+	'''
+	This method calculates 2^i
+	'''
+	return 2 ** i
+
+def two_hoch_thirty_eight():
+	'''
+	This method calculates 2^38
+	'''
+	return two_hoch_etwas(38)
+
+x = two_hoch_thirty_eight
+print(x)
